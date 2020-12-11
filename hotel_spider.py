7064254b8ea8b431335241aaaf314fc951265436
liudy <liudeyin95@163.com>
2020-12-11 23:51:26 +0800

    Add files via upload

---
+++ hotel_spider.py
@@ -0,0 +1,24 @@
+
+# import time
+# import os
+# import csv
+# import logging
+# from pprint import pprint
+# from collections import Counter
+
+# import requests
+# import matplotlib.pyplot as plt
+# import jieba
+# import pymysql
+# from gevent import monkey
+# from gevent.pool import Pool
+# from queue import Queue
+# from bs4 import BeautifulSoup
+
+https://hotels.ctrip.com/hotels/detail/?hotelId=5159100&checkIn=2020-11-29&checkOut=2020-11-30&cityId=933&minprice=&mincurr=&adult=1&children=0&ages=&crn=1&curr=&fgt=&stand=&stdcode=&hpaopts=&mproom=&ouid=&shoppingid=&roomkey=&highprice=-1&lowprice=0&showtotalamt=&hotelUniqueKey=
+
+https://hotels.ctrip.com/hotels/detail/?hotelId=6636356&checkIn=2020-11-29&checkOut=2020-11-30&cityId=933&minprice=&mincurr=&adult=1&children=0&ages=&crn=1&curr=&fgt=&stand=&stdcode=&hpaopts=&mproom=&ouid=&shoppingid=&roomkey=&highprice=-1&lowprice=0&showtotalamt=&hotelUniqueKey=
+
+
+
+https://hotels.ctrip.com/hotels/detail/?hotelId=6636356&checkIn=2020-11-29&checkOut=2020-11-30&cityId=933&minprice=&mincurr=&adult=1&children=0&ages=&crn=1&curr=&fgt=&stand=&stdcode=&hpaopts=&mproom=&ouid=&shoppingid=&roomkey=&highprice=-1&lowprice=0&showtotalamt=&hotelUniqueKey=
